34a7fac424a3edeceb4027a507c4264f65b72591
Evandro Vieira <evandrovieira_mat@gmail.com>
2026-04-24 02:33:48 -0300

    Remove testes da API

--- backend/spec/SpecHelper.php
+++ backend/spec/SpecHelper.php
@@ -1,7 +1,6 @@
 <?php
 namespace App\Test;
 
-use phputil\router\{FakeHttpRequest, FakeHttpResponse, Router};
 use function App\Infra\conectarAoDB;
 
 class SpecHelper {
@@ -11,22 +10,5 @@ class SpecHelper {
         $pdo->exec(file_get_contents($baseDir . 'cefetshop_bd_test.sql'));
         $pdo->exec(file_get_contents($baseDir . 'script_populacao_test.sql'));
         return $pdo;
-    }
-
-    public static function testarRota(string $caminho, string $metodo, string $arquivoRota, \PDO $pdo, array $corpo = []): FakeHttpResponse {
-        $app = new Router();
-        $registrarRotas = require __DIR__ . '/../src/routes/' . $arquivoRota;
-        $registrarRotas($app, $pdo);
-
-        $fakeReq = new FakeHttpRequest();
-        $fakeReq->withURL($caminho)->withMethod($metodo);
-        if (!empty($corpo)) {
-            $fakeReq->withBody($corpo);
-        }
-        
-        $fakeRes = new FakeHttpResponse();
-        $app->listen(['req' => $fakeReq, 'res' => $fakeRes]);
-        
-        return $fakeRes;
     }
 }
